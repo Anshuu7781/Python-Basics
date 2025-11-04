50db6daf4c25a22a8fa839ff2d873994d8b2aa27
Ansh Srivastava <Ansh.sri.7781@gmail.com>
2025-11-04 21:17:17 +0530

    Add files via upload
    Program to find average of two numbers entered by the user.

---
+++ Q5.py
@@ -0,0 +1,4 @@
+# Write a program to find average of two numbers entered by the user
+a = int(input("enter the first number :"))
+b = int(input("enter the second number :"))
+print("the average of the numbers is :", (a+b)/2)
